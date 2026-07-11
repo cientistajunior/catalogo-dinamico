1870b96fc8b856d104958084f0131e691a3f4e40
Wilson Junior <juniorwilson2830@gmail.com>
2026-07-10 23:11:56 -0300

    estrututrando main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+"use Strict";
+const produtos = [
+    { id: 1, nome: "Smartphone Poco F6", categoria: "Celulares", preco: 2500 },
+    { id: 2, nome: "Notebook Ubuntu Dev", categoria: "Computadores", preco: 4500 },
+    { id: 3, nome: "Monitor 4K Pro", categoria: "Monitores", preco: 1800 },
+    { id: 4, nome: "Smartphone Poco F6 Pro", categoria: "Celulares", preco: 3500 }
+];
+
+const filtrarCelulares = () => {};
