00ca3ba7858257a8b81417f512b707f880b9667d
Wilson Junior <juniorwilson2830@gmail.com>
2026-07-12 19:55:11 -0300

    refatorando html css e melhorando main.js

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 "use Strict";
-const produtos = [
-    { id: 1, nome: "Smartphone Poco F6", categoria: "Celulares", preco: 2500 },
-    { id: 2, nome: "Notebook Ubuntu Dev", categoria: "Computadores", preco: 4500 },
-    { id: 3, nome: "Monitor 4K Pro", categoria: "Monitores", preco: 1800 },
-    { id: 4, nome: "Smartphone Poco F6 Pro", categoria: "Celulares", preco: 3500 }
+const products = [
+    { id: 1, name: "Smartphone Poco F6", category: "Celulares", price: 2500 },
+    { id: 2, name: "Notebook Ubuntu Dev", category: "Computadores", price: 4500 },
+    { id: 3, name: "Monitor 4K Pro", category: "Monitores", price: 1800 },
+    { id: 4, name: "Smartphone Poco F6 Pro", category: "Celulares", price: 3500 }
 ];
 
-const filtrarCelulares = () => {};
+
+/**
+ @returns {Array<Object>
+}
+ */
+
+const productNameLabel = document.querySelector('.productNameLabel');
+const productNameInput = document.querySelector('#productNameInput');
+const btnPush = document.querySelector('.btnPush');
+const listProducts = document.querySelector('#list-products');
+
+const filterCellPhones = () => {
+    const shareProduct = products.filter((item) => item.category === "Celulares");
+    return shareProduct;
+};
+
+btnPush.addEventListener("click", (event) => {
+  const filterPhone = filterCellPhones();
+  return renderProducts(filterPhone);
+});
+
+/**
+ * @param {Array<Object>} lista
+ */
+const renderProducts = (list) => {
+    listProducts.innerHTML = "";
+
+    list.forEach((productsList) => {
+        listProducts.innerHTML += `<li>${productsList.name} - R$ ${productsList.price}</li>`;
+    })
+};
