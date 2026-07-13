b2ee0d09e2b859339f840716317886c53cec49c4
Wilson Neves Moreira Junior <wilson.moreira@tse.jus.br>
2026-07-13 19:31:39 -0300

    refatorando html para adiconar botao excluir e ajustando main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 "use Strict";
 const products = [
-    { id: 1, name: "Smartphone Poco F6", category: "Celulares", price: 2500 },
-    { id: 2, name: "Notebook Ubuntu Dev", category: "Computadores", price: 4500 },
-    { id: 3, name: "Monitor 4K Pro", category: "Monitores", price: 1800 },
-    { id: 4, name: "Smartphone Poco F6 Pro", category: "Celulares", price: 3500 }
+    { id: 1, name: "Smartphone Poco F6", category: "celulares", price: 2500 },
+    { id: 2, name: "Notebook Ubuntu Dev", category: "computadores", price: 4500 },
+    { id: 3, name: "Monitor 4K Pro", category: "monitores", price: 1800 },
+    { id: 4, name: "Smartphone Poco F6 Pro", category: "celulares", price: 3500 },
+    { id: 5, name: "Teclado HP", category: "teclados", price: 270.69 },
+    { id: 6, name: "Tablet Samsung S10", category: "tablets", price: 2184.57 },
+    { id: 7, name: "Tablet Xaiomi Redmi Pad 2 11", category: "tablets", price: 1359.21 }
 ];
 
 
@@ -14,7 +17,11 @@ const products = [
 
 const productNameLabel = document.querySelector('.productNameLabel');
 const productNameInput = document.querySelector('#productNameInput');
-const btnPush = document.querySelector('.btnPush');
+const productCategoryLabel = document.querySelector('.productCategoryLabel');
+const productCategoryInput = document.querySelector('#productCategoryInput');
+const productPriceLabel = document.querySelector('.productPriceLabel');
+const productPriceInput = document.querySelector('#productPriceInput');
+const btnAdd = document.querySelector('.btnAdd');
 const listProducts = document.querySelector('#list-products');
 
 const filterCellPhones = () => {
@@ -22,18 +29,51 @@ const filterCellPhones = () => {
     return shareProduct;
 };
 
-btnPush.addEventListener("click", (event) => {
-  const filterPhone = filterCellPhones();
-  return renderProducts(filterPhone);
+btnAdd.addEventListener("click", (event) => {
+    /** para evitar evento inisperado do botão. Uma boa prática. */
+    event.preventDefault();
+
+    const name = productNameInput.value;
+    const category = productCategoryInput.value;
+    const price = productPriceInput.value;
+
+     /** proibe adicionar caso esteja vazio os inputs. */
+    if(!name || !category || !price){
+        alert("Por favor, preencha todos os campos!")
+        return
+    }
+
+    const newProduct = {
+        id: products.length + 1,
+        name: name,
+        category: category,
+        price: price,
+    }
+
+    products.push(newProduct)
+    renderProducts(products);
+
+    /**resetar os campos dos inputs */
+    productNameInput.value = "";
+    productCategoryInput.value = "";
+    productPriceInput.value = "";
 });
 
 /**
- * @param {Array<Object>} lista
+ * @param {Array<Object>} list
  */
 const renderProducts = (list) => {
     listProducts.innerHTML = "";
 
     list.forEach((productsList) => {
-        listProducts.innerHTML += `<li>${productsList.name} - R$ ${productsList.price}</li>`;
+        listProducts.innerHTML += `<li>${productsList.name} - R$ ${productsList.price} <button class="btnDelete">Excluir</button></li>`;
     })
 };
+
+/**Pra mostrar a lista de produtos */
+renderProducts(products);
+
+listProducts.addEventListener("click", (event) => {
+    // Aqui dentro vamos descobrir exatamente QUAL elemento foi clicado!
+    console.log(event.target);
+});
